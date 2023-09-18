75cb5fc38fe9d093c392c83b3d46f39daf7fec6b
Kristin Baumann <mail@kristin-baumann.com>
2023-09-18 21:24:36 +0200

    Added circle grid

---
+++ src/routes/Chart.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import data from '../data/temp_country_weekly_median_column.json';
+
+	const days = data.Date;
+	const weekTempCountry = data.DE;
+	console.log('weekTempCountry', weekTempCountry);
+</script>
+
+<div>
+	<p>Chart</p>
+	<svg width="2000" height="2000">
+		<g transform={`translate(10,10)`}>
+			{#each weekTempCountry as weekTemp, i}
+				<circle
+					cx={15 * (i % 53)}
+					cy={Math.floor(i / 53) * 15}
+					r="5"
+					fill="black"
+					stroke="black"
+					aria-label={`${i}-${days[i]}`}
+				/>
+			{/each}
+		</g>
+	</svg>
+</div>
