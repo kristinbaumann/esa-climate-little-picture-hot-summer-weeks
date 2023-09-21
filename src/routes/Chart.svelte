39eab25ef877c8a543bc8b42367465fb06792eb5
Kristin Baumann <mail@kristin-baumann.com>
2023-09-21 15:09:28 +0200

    Fixed date calculation

--- src/routes/Chart.svelte
+++ src/routes/Chart.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import data from '../data/temp_country_weekly_median_column.json';
+	import data from '../data/temp_country_weekly_mean_week_aggregation_start_monday.json';
+
 	import { scaleLinear } from 'd3-scale';
-	import { schemeGreens, interpolateSpectral } from 'd3-scale-chromatic';
+	import { interpolateSpectral } from 'd3-scale-chromatic';
 
-	const scale = schemeGreens[6];
-
-	const days = data.Date;
-	const weekTempCountry = data.DE;
-
-	const tempAsNumber = weekTempCountry.map((d: String) => Number(d));
-	console.log('temp', tempAsNumber, interpolateSpectral(1));
+	const tempAsNumber = data.map((d: any) => Number(d.DE));
 
 	const myScale = scaleLinear()
 		.domain([Math.min.apply(Math, tempAsNumber), Math.max.apply(Math, tempAsNumber)])
@@ -18,15 +13,16 @@
 
 <svg width="2000" height="2000">
 	<g transform={`translate(10,10)`}>
-		{#each tempAsNumber as weekTemp, i}
-			<circle
-				cx={15 * (i % 52)}
-				cy={(new Date(days[i]).getFullYear() - 1979) * 15}
-				r="5"
-				fill={interpolateSpectral(myScale(weekTemp))}
-				aria-label={`${i}-${days[i]}-${weekTemp}`}
-			/>
-			<!-- aria-label={`${1980-}`} -->
+		{#each data as entry, i}
+			{#if entry.weeknumber !== 53 && entry.year < 2023}
+				<circle
+					cx={15 * entry.weeknumber}
+					cy={(entry.year - 1979) * 15}
+					r="5"
+					aria-label={`${entry.year} ${entry.weeknumber} ${entry.DE}`}
+					fill={interpolateSpectral(myScale(Number(entry.DE)))}
+				/>
+			{/if}
 		{/each}
 	</g>
 </svg>
