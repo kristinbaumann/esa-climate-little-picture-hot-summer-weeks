a6cf9a7d626fb782d6f50a3e0e29f7cdd1150a17
Kristin Baumann <mail@kristin-baumann.com>
2023-09-22 11:20:27 +0200

    Added jupyter notebook with max daily values

--- src/routes/Chart.svelte
+++ src/routes/Chart.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
-	import data from '../data/temp_country_weekly_max.json';
+	import data from '../data/temp_country_weekly_max_daily_in_week_DE.json';
 
-	import { scaleLinear } from 'd3-scale';
-	import { interpolateSpectral } from 'd3-scale-chromatic';
+	// import { scaleLinear } from 'd3-scale';
+	// import { interpolateSpectral } from 'd3-scale-chromatic';
 
-	const tempAsNumber = data.map((d: any) => Number(d.DE));
+	// const tempAsNumber = data.map((d: any) => Number(d.max_daily_in_week));
 
-	const myScale = scaleLinear()
-		.domain([Math.min.apply(Math, tempAsNumber), Math.max.apply(Math, tempAsNumber)])
-		.range([1, 0]);
+	// const myScale = scaleLinear()
+	// 	.domain([Math.min.apply(Math, tempAsNumber), Math.max.apply(Math, tempAsNumber)])
+	// 	.range([1, 0]);
+
+	function getColor(temp: number) {
+		// return interpolateSpectral(myScale(temp));
+
+		if (temp > 24) {
+			return 'darkred';
+		}
+		return '#eeeeee';
+	}
 </script>
 
 <svg width="2000" height="2000">
@@ -19,8 +28,8 @@
 					cx={15 * entry.weeknumber}
 					cy={(entry.year - 1979) * 15}
 					r="5"
-					aria-label={`${entry.year} ${entry.weeknumber} ${entry.DE}`}
-					fill={interpolateSpectral(myScale(Number(entry.DE)))}
+					aria-label={`${entry.year} ${entry.weeknumber} ${entry.max_daily_in_week}`}
+					fill={getColor(Number(entry.max_daily_in_week))}
 				/>
 			{/if}
 		{/each}
