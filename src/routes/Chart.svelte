3c722ce066d928772cc451d28c7b2f5a3a1888af
Kristin Baumann <mail@kristin-baumann.com>
2023-09-22 12:09:47 +0200

    Added max daily calculation for all countries

--- src/routes/Chart.svelte
+++ src/routes/Chart.svelte
@@ -1,5 +1,45 @@
 <script lang="ts">
-	import data from '../data/temp_country_weekly_max_daily_in_week_DE.json';
+	import data from '../data/temp_country_weekly_max_daily_in_week_all_countries.json';
+
+	const countryNames = [
+		'AL',
+		'AT',
+		'BA',
+		'BE',
+		'BG',
+		'CH',
+		'CY',
+		'CZ',
+		'DE',
+		'DK',
+		'EE',
+		'EL',
+		'ES',
+		'FI',
+		'FR',
+		'HR',
+		'HU',
+		'IE',
+		'IS',
+		'IT',
+		'LT',
+		'LU',
+		'LV',
+		'ME',
+		'MK',
+		'NL',
+		'NO',
+		'PL',
+		'PT',
+		'RO',
+		'RS',
+		'SE',
+		'SI',
+		'SK',
+		'TR',
+		'UK'
+	]; // no data for LI and MT
+	//const countryName = 'BE';
 
 	// import { scaleLinear } from 'd3-scale';
 	// import { interpolateSpectral } from 'd3-scale-chromatic';
@@ -13,25 +53,33 @@
 	function getColor(temp: number) {
 		// return interpolateSpectral(myScale(temp));
 
-		if (temp > 24) {
+		if (temp > 25) {
 			return 'darkred';
+		}
+		if (temp > 20) {
+			return 'indianred';
 		}
 		return '#eeeeee';
 	}
 </script>
 
-<svg width="2000" height="2000">
-	<g transform={`translate(10,10)`}>
-		{#each data as entry, i}
-			{#if entry.weeknumber !== 53 && entry.year < 2023}
-				<circle
-					cx={15 * entry.weeknumber}
-					cy={(entry.year - 1979) * 15}
-					r="5"
-					aria-label={`${entry.year} ${entry.weeknumber} ${entry.max_daily_in_week}`}
-					fill={getColor(Number(entry.max_daily_in_week))}
-				/>
-			{/if}
-		{/each}
-	</g>
-</svg>
+{#each countryNames as countryName}
+	<svg width="1500" height="750">
+		<g transform={`translate(15,15)`}>
+			<text>{countryName}</text>
+		</g>
+		<g transform={`translate(5,25)`}>
+			{#each data as entry, i}
+				{#if entry.weeknumber !== 53 && entry.year < 2023}
+					<circle
+						cx={15 * entry.weeknumber}
+						cy={(entry.year - 1979) * 15}
+						r="5"
+						aria-label={`${entry.year} ${entry.weeknumber} ${entry[countryName]}`}
+						fill={getColor(Number(entry[countryName]))}
+					/>
+				{/if}
+			{/each}
+		</g>
+	</svg>
+{/each}
