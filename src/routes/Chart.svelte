7919006b1b524e5087bae6e0369a0c7e697e08a7
Kristin Baumann <mail@kristin-baumann.com>
2023-11-03 15:06:34 +0100

    Final version for CLIP competition

--- src/routes/Chart.svelte
+++ src/routes/Chart.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	// import data from '../data/temp_country_weekly_max_daily_in_week_all_countries_new.json';
-	import data from '../data/temp_subcountry_ES30_weekly_max_daily_in_week.json';
+	import data from '../data/temp_country_weekly_max_daily_in_week_all_countries_new.json';
+	//import data from '../data/temp_subcountry_ES30_weekly_max_daily_in_week.json';
 
 	import { scaleSequential } from 'd3-scale';
 	import { interpolateReds } from 'd3-scale-chromatic';
@@ -44,17 +44,18 @@
 		// 'UK'
 	]; // no data for LI and MT
 
-	countryNames = ['max_daily_in_week'];
+	countryNames = ['SK'];
 	const colorGrey = '#dfdfdf';
 	const distanceBetweenCircles = 4;
 	const radius = 5;
-	const minTemp = 26;
+	const minTemp = 20.5;
 
-	const test = data.map((d: any) => Number(d['max_daily_in_week']));
+	const test = data.map((d: any) => Number(d['SK']));
 	const maxTemp = Math.max.apply(Math, test);
 	console.log(maxTemp);
 
-	const color = scaleSequential(interpolateReds).domain([minTemp, maxTemp]);
+	//const color = scaleSequential(interpolateReds).domain([minTemp, maxTemp]);
+	const color = scaleSequential(interpolateReds).domain([minTemp - 0.5, maxTemp - 0.5]);
 
 	function getFill(temp: number) {
 		if (temp < minTemp) return colorGrey;
@@ -73,12 +74,12 @@
 
 {#each countryNames as countryName}
 	<svg width="920" height="950" style={'background-color: #FFFCED;'}>
-		<g transform={`translate(15,15)`}>
+		<!-- <g transform={`translate(15,15)`}>
 			<text>{countryName}</text>
-		</g>
+		</g> -->
 		<g transform={`translate(15,25)`}>
 			{#each data as entry}
-				{#if entry.weeknumber !== 53}
+				{#if entry.weeknumber !== 53 && entry.year > 1979 && entry.year < 2023}
 					<circle
 						cx={(radius * 2 + distanceBetweenCircles) * entry.weeknumber}
 						cy={(entry.year - 1979) * (radius * 2 + distanceBetweenCircles * 2)}
